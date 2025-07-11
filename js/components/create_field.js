a4eaca7026511dae36eae27e987c8eadcd940456
DanyaPe <denzed5221@gmail.com>
2025-07-12 01:12:15 +0700

    style: Оформление модального окна
    - У функции создания поля изменено имя атрибута
    - Перемещены кнопки на форме
    - Небольшое обрамление модального окна

--- js/components/create_field.js
+++ js/components/create_field.js
@@ -2,18 +2,18 @@ import format_date from "../format_date.js";
 
 /**
  * Функция создания именованного поля для ввода и хранения данных. Поле формата HTML-элемента "input" вложенного в "label".
- * @param {string} text - Наименование поля, будет отображаться как "label"
+ * @param {string} name - Наименование поля, будет отображаться как "label"
  * @param {?(string|Date)} value - Значение, которое будет указано в поле
  * @param {?string} id - Идентификатор для поиска поля в документе
  * @returns {HTMLLabelElement}
  */
-function create_field(text, value, id) {
-    if (!(typeof text === 'string')) {
-        console.error(`Указание наименование поля должно быть в формате строки:\n${text}\nОшибка при создании поля`);
+function create_field(name, value, id) {
+    if (!(typeof name === 'string')) {
+        console.error(`Указание наименование поля должно быть в формате строки:\n${name}\nОшибка при создании поля`);
         return;
     };
     const label = document.createElement('label');
-    label.textContent = text;
+    label.textContent = name;
     const input = document.createElement('input');
     if (!value || value === undefined) {
         input.type = 'text';
