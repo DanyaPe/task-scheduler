faa1d3578b477fc7086fa1a0009f26e742f28b6c
DanyaPe <denzed5221@gmail.com>
2025-07-30 14:32:06 +0700

    refactor: Добавлена дополнительная проверка на соответствие данных формату даты

--- js/format_date.js
+++ js/format_date.js
@@ -1,10 +1,10 @@
 /**
- * Функция форматирования даты к виду "datetime-local"
+ * Функция форматирования даты к строчному виду "datetime-local"
  * @param {!Date} date - Объект Date
- * @returns {string} Дата в формате `yyyy-mm-ddThh:mm`
+ * @returns {string} Строчное представление даты в формате `yyyy-mm-ddThh:mm`
  */
 function format_date(date) {
-    if (date && !isNaN(date)) {
+    if (date && !isNaN(date) && date instanceof Date) {
         const day = date.getDate().toString().padStart(2,0);
         const month = (date.getMonth() + 1).toString().padStart(2,0);
         const year = date.getFullYear();
