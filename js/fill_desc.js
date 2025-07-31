510516d76e9cb51a30dfe3e988fde28e437047ce
DanyaPe <denzed5221@gmail.com>
2025-08-01 02:13:59 +0700

    fix: Исправление обращения к свойству статуса в функции "fill_desc"

--- js/fill_desc.js
+++ js/fill_desc.js
@@ -8,7 +8,7 @@ function fill_desc() {
     for (let index = 0; index < Storage.length; index++) {
         const Key = Storage.key(index);
         try {
-            JSON.parse(Storage.getItem(Key))['Статус задачи'] !== 'Решена' ? NewTaskList.appendChild(create_task_element(Key)) : ResolvedTaskList.appendChild(create_task_element(Key)); 
+            JSON.parse(Storage.getItem(Key)).status !== 'Решена' ? NewTaskList.appendChild(create_task_element(Key)) : ResolvedTaskList.appendChild(create_task_element(Key)); 
         } catch (error) {
             console.error(`${error}\nОшибка формирования задачи по ключу хранилища: ${Key}`);
             continue
