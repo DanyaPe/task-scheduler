e60bd3d5e5c653d10f712b43563a9c29ce757ed9
DanyaPe <denzed5221@gmail.com>
2025-08-11 15:39:16 +0700

    refactor: Убрана кнопка "Создать задачу"
    Логика создания задачи перенесена с кнопки на кликабельный блок на доске новых задач.
    Новые задачи будут вносится на доску новых задач после этого блока.
    На доски добавлено их наименование в виде обычного текста.

--- js/fill_desc.js
+++ js/fill_desc.js
@@ -1,4 +1,4 @@
-import { NewTaskList, ResolvedTaskList, Storage } from "./data.js";
+import { OpenModal, ResolvedTaskList, Storage } from "./data.js";
 import create_task_element from "./components/create_task_element.js";
 
 /**
@@ -8,7 +8,7 @@ function fill_desc() {
     for (let index = 0; index < Storage.length; index++) {
         const Key = Storage.key(index);
         try {
-            JSON.parse(Storage.getItem(Key)).status !== 'Решена' ? NewTaskList.appendChild(create_task_element(Key)) : ResolvedTaskList.appendChild(create_task_element(Key)); 
+            JSON.parse(Storage.getItem(Key)).status !== 'Решена' ? OpenModal.after(create_task_element(Key)) : ResolvedTaskList.appendChild(create_task_element(Key)); 
         } catch (error) {
             console.error(`${error}\nОшибка формирования задачи по ключу хранилища: ${Key}`);
             continue
